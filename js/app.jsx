20b367c6af0d31784f64949a4085ad6a9fe5a7e1
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2016-02-01 22:27:37 +0300

    Added Image component example and centered content

--- js/app.jsx
+++ js/app.jsx
@@ -1,9 +1,37 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Email, Button } from 'ink-react'
+import {
+  Email,
+  Button,
+  Row, Col,
+  Container,
+  Image
+} from 'ink-react'
 
 ReactDOM.render((
   <Email>
-    <Button class="success round" href="#">Click me</Button>
+    <Container>
+      <Row>
+        <Col width="six">
+          <Button class="success round" href="#">Click me</Button>
+        </Col>
+        <Col center panel width="three">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Col>
+        <Col width="three">
+          <Button class="alert" href="#">Click me</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col center width="six">
+          <Image src="http://placehold.it/125x125&text=Centered%20Image"/>
+        </Col>
+        <Col center width="six">
+          <Button className="hello" href="#">Hello</Button>
+        </Col>
+      </Row>
+    </Container>
   </Email>
 ), document.getElementById('main'))
